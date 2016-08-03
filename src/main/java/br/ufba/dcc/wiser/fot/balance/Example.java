645b54f15b5f538f1503d92626587da2be8139e2
jurandirbarbosa <jurandirbarbosa@uol.com.br>
2016-08-03 19:06:26 -0300

    Código 'mais enxuto', sem a necessidade do singleton.

--- src/main/java/br/ufba/dcc/wiser/fot/balance/Example.java
+++ src/main/java/br/ufba/dcc/wiser/fot/balance/Example.java
@@ -3,26 +3,17 @@ package br.ufba.dcc.wiser.fot.balance;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.Cluster;
 import com.hazelcast.core.Member;
-//import java.util.ArrayList;
-//import java.util.List;
+import org.apache.karaf.cellar.hazelcast.HazelcastNode;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-//import org.apache.karaf.cellar.core.discovery.DiscoveryService;
-//import org.apache.karaf.cellar.core.utils.CombinedClassLoader;
-import org.apache.karaf.cellar.hazelcast.HazelcastNode;
-import org.apache.karaf.cellar.hazelcast.SingletonStaticFactory;
-import org.apache.karaf.cellar.hazelcast.factory.HazelcastConfigurationManager;
-import org.apache.karaf.cellar.hazelcast.factory.HazelcastServiceFactory;
-//import org.osgi.framework.ServiceReference;
-//import org.osgi.util.tracker.ServiceTracker;
-//import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 
 public class Example {
 
     private String a;
-    private String b;  
+    private String b;
+    private HazelcastInstance instance = null;
     
     public void setA(String a) {
         this.a = a;
@@ -35,43 +26,38 @@ public class Example {
     public void bla() {
         System.out.println("Started Example: " + a + " " + b);
     }
+    
+    public void setInstance(HazelcastInstance instance){
+        this.instance = instance;
+    }
 
     public void bli(){
 
-        System.out.println("Listing node: ");
-
-        if(SingletonStaticFactory.getInstance() == null){
+        System.out.println("Listing node(s): ");
+        
+        // Check if there's a instance of hazelcast
+        if(instance == null){
             System.out.println("erro 1");
             return;
         }
+
+        // Get Cluster with the hazelcast instance
+        Cluster cluster = instance.getCluster();
         
-        HazelcastServiceFactory factory = SingletonStaticFactory.getInstance();
-        HazelcastInstance factoryInstance = null;
-        try {
-            factoryInstance = factory.getInstance();
-        } catch (InterruptedException ex) {
-            System.out.println("erro 2");
-            Logger.getLogger(Example.class.getName()).log(Level.SEVERE, null, ex);
-            return;
-        }
-        
-        Cluster cluster = factoryInstance.getCluster();
-        cluster.getMembers();
-        
-        if (cluster != null) {
+        // Get all members of the Hazelcast Cluster and display some properties
+        try{
             Set<Member> members = cluster.getMembers();
             if (members != null && !members.isEmpty()) {
                 for (Member member : members) {
                     HazelcastNode node = new HazelcastNode(member);
-                    System.out.println( //"ID = " + node.getId() +
-                                        //", NODE = " + node.getHost() +
-                                        //", ALIAS = " + node.getAlias() +
-                                        ", OBJ = " + node.toString());
+                    
+                    System.out.println("OBJ = " + node.toString());
                 }
             }
         }
-        else{
-            System.out.println("erro 3");
+        catch(NullPointerException ex){
+            System.out.println("erro 2");
+            Logger.getLogger(Example.class.getName()).log(Level.SEVERE, null, ex);
         }
     
     }
