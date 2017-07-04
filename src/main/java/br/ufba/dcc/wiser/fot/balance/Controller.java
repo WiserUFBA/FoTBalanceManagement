afc7d7ef53158e91f4c5533e2b5adf582729bf75
jefersonla <oi_je@hotmail.com>
2017-07-04 10:57:02 -0300

    Added log about empty groups

--- src/main/java/br/ufba/dcc/wiser/fot/balance/Controller.java
+++ src/main/java/br/ufba/dcc/wiser/fot/balance/Controller.java
@@ -581,7 +581,8 @@ public class Controller {
             Group actual_group = group_list.get(group_name);
 
             /* If actual group is empty avoid call solve, and skip this bundle */
-            if(actual_group.getHostList().size() == 0){
+            if(actual_group.getHostList().isEmpty()){
+                FoTBalanceUtils.info("Empty group avoiding...");
                 continue;
             }
             
